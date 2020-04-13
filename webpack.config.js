e0af75c62abdfa1ebc3c7b78c1d988ebafc9435c
Dakota Gilbreath <46034431+dgilbrea92@users.noreply.github.com>
2020-04-13 16:53:44 -0700

    initialized boilerplate

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+
+module.exports = {
+  mode: process.env.NODE_ENV,
+  entry: './client/index.js',
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: 'bundle.js',
+  },
+  devServer: {
+    publicPath: '/build',
+    proxy: {
+      '/api': 'http://localhost:3000'
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: [
+          path.resolve(__dirname, "node_modules")
+        ],
+        loader: "babel-loader",
+        options: {
+          presets: [
+            '@babel/preset-env',
+            '@babel/preset-react'
+          ]
+        },
+      },
+      {
+        test: /\.*css/,
+        exclude: [
+          path.resolve(__dirname, 'node_modules' )
+        ],
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+        ],
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".js", ".json", ".jsx", ".css"],
+  }
+}
