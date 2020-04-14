b3bb08f7fd394bf805df11af20dad983c0cbdecc
Jesse Zuniga <jzuniga206@gmail.com>
2020-04-13 17:34:17 -0700

    Updated file structure for Redux

---
+++ client/reducers/index.js
@@ -0,0 +1,9 @@
+import { combineReducers } from 'redux';
+
+// import reducer(s) function(s) once written
+
+const reducers = combineReducers({
+  // name for reducers : // imported reducers function
+});
+
+export default reducers;
